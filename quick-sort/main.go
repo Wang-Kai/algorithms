fa3fe309e15a1278d80d02c4ac24af68be42722f
Steve <kai.wang@ucloud.cn>
2020-02-04 22:13:10 +0800

    添加随机选择主元版本的快排序

--- quick-sort/main.go
+++ quick-sort/main.go
@@ -1,10 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
-	var arr = []int{34, 9, 0, 3, 4, 5, 43, 90, 23, 283458, 3, 6, 3456}
-
+	var arr = []int{34, 9, 0, 3, 4, 5, 43, 90, 23, 28, 923328, 48923, 3, 458, 3, 6, 345, 6}
 	quickSort(arr, 0, len(arr)-1)
 
 	fmt.Println(arr)
@@ -12,13 +15,17 @@ func main() {
 
 func quickSort(arr []int, from, to int) {
 	if from < to {
-		q := partition(arr, from, to)
+
+		q := randomizedPartiion(arr, from, to)
+
 		quickSort(arr, from, q-1)
 		quickSort(arr, q+1, to)
 	}
 }
 
 func partition(arr []int, from, to int) int {
+	fmt.Printf("Handled Array ==> %v\n", arr)
+
 	key := arr[to]
 
 	var i = from - 1
@@ -32,4 +39,14 @@ func partition(arr []int, from, to int) int {
 
 	arr[i+1], arr[to] = arr[to], arr[i+1]
 	return i + 1
+}
+
+// randomizedPartiion 将随机的选择主元
+func randomizedPartiion(arr []int, from, to int) int {
+	rand.Seed(time.Now().Unix())
+	randomIndex := rand.Intn(to-from+1) + from
+
+	arr[randomIndex], arr[to] = arr[to], arr[randomIndex]
+
+	return partition(arr, from, to)
 }
